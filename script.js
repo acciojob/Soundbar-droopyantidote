a9b55f069eb91b6d6dd3dbd6a7ed84eccaae2bce
droopyantidote <144815313+droopyantidote@users.noreply.github.com>
2023-11-21 12:21:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+// Function to play a sound
+function playSound(soundFileName) {
+    var audio = new Audio('sounds/' + soundFileName);
+    audio.play();
+}
+
+// Function to stop all sounds
+function stopAllSounds() {
+    var sounds = document.querySelectorAll('audio');
+    sounds.forEach(function(sound) {
+        sound.pause();
+        sound.currentTime = 0;
+    });
+}
